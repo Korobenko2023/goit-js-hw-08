59dda0a6228d8a2ea8683c0adaa0520450d7871f
Korobenko2023 <vivika3110@gmail.com>
2023-09-10 21:45:07 +0300

    task-3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,9 @@
  import Player from '@vimeo/player';
  import throttle from 'lodash.throttle';
 
- try {
   const iframe = document.querySelector('iframe');
   const player = new Player(iframe);
   const savedTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
- 
   if (savedTime !== null) {
     player.setCurrentTime(savedTime);
   }
@@ -18,26 +16,7 @@
     const currentTime = data.seconds;
     updateLocalStorage(currentTime);
   });
- 
- } catch (error) {
-   console.error('Сталася помилка:', error);
- }
-
-    // import Player from '@vimeo/player';
-    // import throttle from 'lodash.throttle';
-    // const iframe = document.querySelector('iframe');
-    // const player = new Player(iframe);
-    // const savedTime = localStorage.getItem('videoplayer-current-time');
-    // if (savedTime !== null) {
-    //  player.setCurrentTime(parseFloat(savedTime));
-    // }
-    // const updateLocalStorage = throttle(function(currentTime) {
-    // localStorage.setItem('videoplayer-current-time', currentTime);
-    // }, 1000);
   
-    // player.on('timeupdate', function(data) {
-    // const currentTime = data.seconds;
-    // updateLocalStorage(currentTime);
-    // });
+    
 
 
