f7a6718a5d96f56e89b12abab5ef911b6fafda3a
Korobenko2023 <vivika3110@gmail.com>
2023-09-14 22:24:20 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -36,3 +36,42 @@ function updateFormFields() {
       });
           
       updateFormFields();
+
+// import throttle from 'lodash.throttle';
+
+// const registerForm = document.querySelector(".feedback-form");
+// const emailInput = registerForm.querySelector('[name="email"]');
+// const messageInput = registerForm.querySelector('[name="message"]');
+
+// function saveInput() {
+//     const feedbackData = {
+//         email: emailInput.value,
+//         message: messageInput.value
+//       };
+//     localStorage.setItem("feedback-form-state", JSON.stringify(feedbackData));
+//     }
+    
+// function updateFormFields() {
+//     const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
+//     if (savedData !== null) {
+//         emailInput.value = savedData.email;
+//         messageInput.value = savedData.message;
+//         }
+//     }
+
+//     function clearLocalStorage() {
+//     localStorage.removeItem("feedback-form-state");
+//     registerForm.reset();
+//     }
+
+//     registerForm.addEventListener('input', throttle(saveInput), 500);
+//     registerForm.addEventListener("submit", function (event) {
+//         event.preventDefault();
+//         console.log({
+//             email: emailInput.value,
+//             message: messageInput.value
+//           });
+//           clearLocalStorage();
+//       });
+          
+//       updateFormFields();
